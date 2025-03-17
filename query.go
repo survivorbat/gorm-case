bfe3b0fb4e2be0a457146d50308e67006ad288f7
marloploemen <marlo.ploemen@gmail.com>
2025-03-17 09:34:30 +0100

    go get -u, fix tablename, fix tests

--- query.go
+++ query.go
@@ -2,6 +2,8 @@ package gormcase
 
 import (
 	"fmt"
+	"strings"
+
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 )
@@ -27,76 +29,102 @@ func (d *gormCase) queryCallback(db *gorm.DB) {
 	}
 
 	for index, cond := range exp.Exprs {
-		switch cond := cond.(type) {
-		case clause.Eq:
-			if d.conditionalTag {
-				columnName, ok := cond.Column.(string)
-				if !ok {
-					continue
-				}
+		if expression := d.handleExpression(db, cond); expression != nil {
+			exp.Exprs[index] = expression
+		}
+	}
+}
 
-				value := db.Statement.Schema.FieldsByDBName[columnName].Tag.Get(tagName)
-
-				// Ignore if there's no valid tag value
-				if value != "true" {
-					continue
-				}
+func (d *gormCase) handleExpression(db *gorm.DB, cond clause.Expression) clause.Expression {
+	switch cond := cond.(type) {
+	case clause.Eq:
+		if d.conditionalTag {
+			columnName, ok := cond.Column.(string)
+			if !ok {
+				return nil
 			}
 
-			value, ok := cond.Value.(string)
+			table := db.Statement.Schema.Table + "."
+			if strings.HasPrefix(columnName, table) {
+				columnName = strings.TrimPrefix(columnName, table)
+			}
+
+			value := db.Statement.Schema.FieldsByDBName[columnName].Tag.Get(tagName)
+
+			// Ignore if there's no valid tag value
+			if value != "true" {
+				return nil
+			}
+		}
+
+		value, ok := cond.Value.(string)
+		if !ok {
+			return nil
+		}
+
+		condition := fmt.Sprintf("UPPER(%s) = UPPER(?)", cond.Column)
+
+		return db.Session(&gorm.Session{NewDB: true}).Where(condition, value).Statement.Clauses["WHERE"].Expression
+	case clause.IN:
+		if d.conditionalTag {
+			columnName, ok := cond.Column.(string)
+			if !ok {
+				return nil
+			}
+
+			table := db.Statement.Schema.Table + "."
+			if strings.HasPrefix(columnName, table) {
+				columnName = strings.TrimPrefix(columnName, table)
+			}
+
+			value := db.Statement.Schema.FieldsByDBName[columnName].Tag.Get(tagName)
+
+			// Ignore if there's no valid tag value
+			if value != "true" {
+				return nil
+			}
+		}
+
+		var caseCounter int
+		var useOr bool
+
+		query := db.Session(&gorm.Session{NewDB: true})
+
+		for _, value := range cond.Values {
+			value, ok := value.(string)
 			if !ok {
 				continue
 			}
 
+			caseCounter++
+
 			condition := fmt.Sprintf("UPPER(%s) = UPPER(?)", cond.Column)
 
-			exp.Exprs[index] = db.Session(&gorm.Session{NewDB: true}).Where(condition, value).Statement.Clauses["WHERE"].Expression
-		case clause.IN:
-			if d.conditionalTag {
-				columnName, ok := cond.Column.(string)
-				if !ok {
-					continue
-				}
-
-				value := db.Statement.Schema.FieldsByDBName[columnName].Tag.Get(tagName)
-
-				// Ignore if there's no valid tag value
-				if value != "true" {
-					continue
-				}
-			}
-
-			var caseCounter int
-			var useOr bool
-
-			query := db.Session(&gorm.Session{NewDB: true})
-
-			for _, value := range cond.Values {
-				value, ok := value.(string)
-				if !ok {
-					continue
-				}
-
-				caseCounter++
-
-				condition := fmt.Sprintf("UPPER(%s) = UPPER(?)", cond.Column)
-
-				if useOr {
-					query = query.Or(condition, value)
-					continue
-				}
-
-				query = query.Where(condition, value)
-				useOr = true
-			}
-
-			// Do nothing if no changes were made
-			if caseCounter == 0 {
+			if useOr {
+				query = query.Or(condition, value)
 				continue
 			}
 
-			// TODO: Determine whether this is efficient
-			exp.Exprs[index] = db.Session(&gorm.Session{NewDB: true}).Where(query).Statement.Clauses["WHERE"].Expression
+			query = query.Where(condition, value)
+			useOr = true
 		}
+
+		// Do nothing if no changes were made
+		if caseCounter == 0 {
+			return nil
+		}
+
+		// TODO: Determine whether this is efficient
+		return db.Session(&gorm.Session{NewDB: true}).Where(query).Statement.Clauses["WHERE"].Expression
+	case clause.AndConditions:
+		for index, nested := range cond.Exprs {
+			if expression := d.handleExpression(db, nested); expression != nil {
+				cond.Exprs[index] = expression
+			}
+		}
+
+		return cond
+	default:
+		return nil
 	}
 }
